0f3714cbbbecc79d5cd3c73cf3d7b20dc0059a8f
Maiko Shima <93574961+maikoshima@users.noreply.github.com>
2024-01-29 15:40:57 +0900

    Update corstest02

--- src/corstest02/corstest02.js
+++ src/corstest02/corstest02.js
@@ -1,6 +1,5 @@
 fetch("./corstest02.json")
   .then(response => {
-    response.set("Access-Control-Allow-Origin", "https://nice-pebble-01aeed500.4.azurestaticapps.net");
     return response.json();
   })
   .then(data => {
