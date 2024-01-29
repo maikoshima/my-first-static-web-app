38f32f1e16671c10e6b57adb90f362e068866e32
Maiko Shima <93574961+maikoshima@users.noreply.github.com>
2024-01-29 15:33:29 +0900

    Update corstest02.js

--- src/corstest02/corstest02.js
+++ src/corstest02/corstest02.js
@@ -1,11 +1,10 @@
 fetch("./corstest02.json")
   .then(response => {
+    response.set("Access-Control-Allow-Origin", "https://nice-pebble-01aeed500.4.azurestaticapps.net");
     return response.json();
   })
   .then(data => {
     console.log(data);
-    const myHeaders = new Headers();
-    myHeaders.set("Access-Control-Allow-Origin", "https://nice-pebble-01aeed500.4.azurestaticapps.net");
   })
   .catch(error => {
     console.log("fetch failed");
